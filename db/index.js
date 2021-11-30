c54f94f00b016b03dfe28ca9b4b1c71dff623e5a
jaskaur18 <51957556+jaskaur18@users.noreply.github.com>
2021-11-30 19:12:46 +0530

    Update index.js
    remove database url endpoint

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { errorHandler } = require('../helpers')
 const MongoClient = require("mongodb").MongoClient;
 
 const uri =
-    "mongodb+srv://testuser:9dzeCguIGfP9nV4J@cluster0.nr0ri.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-3lup83-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+    "";
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
